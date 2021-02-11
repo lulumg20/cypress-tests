de762cc5919ca3b9a68ea6f17f1a1fb1c40c0e39
Lulu Guo <xguo20me@gmail.com>
2021-02-11 11:19:02 -0500

    cleanup env file

--- cypress/integration/add_new_instructor_test.js
+++ cypress/integration/add_new_instructor_test.js
@@ -1,17 +1,16 @@
 
 // @author: luluguo
 //This script is a functional test written with cypress-test-automation framework for Thinkific web-app platform
-
+//It includes the test cases- Add New Instructor, Update Instructor, Search Instructor
   
   beforeEach(() => {
-    cy.visit('https://www.thinkific.com/signin/')
-      
+    cy.visit(Cypress.env('login_url'))
       cy.get('#email')
-        .type('xinluguo@gmail.com')
-        .should('have.value', 'xinluguo@gmail.com')
+        .type(Cypress.env('test_login_email'))
+        .should('have.value', Cypress.env('test_login_email'))
       cy.get('#password')
-        .type('thinkific2021')
-        .should('have.value', 'thinkific2021')
+        .type(Cypress.env('test_login_password'))
+        .should('have.value', Cypress.env('test_login_password'))
       cy.xpath('//*[@class="button button--icon button--icon--right signin__form__button"]').click()
       cy.url().should('include', '/manage')
   })
@@ -20,39 +19,32 @@
   describe('Navigate Instructors Page', () => {
 
     it('Correct page url', () => {
-      var home_url = cy.url()
-      var instructor_page_url = home_url + '/instructors'
-      cy.log(instructor_page_url)
-      // cy.visit('/instructors')
-
-      //NOTE:should not be hardcoded but code above was not working in cypress runner
-      cy.visit('https://lulu-s-school-031a.thinkific.com/manage/instructors')
+      var instructor_page_url = Cypress.env('base_url') + '/manage/instructors'
+      cy.visit(instructor_page_url)
       cy.url().should('include', '/manage/instructors')
       
     })
   })
   describe('Add a New Instructor Page', () => {
     it('Created a new instructor', () => {
-      var new_instructor_url = "/manage/instructors/new"
-      // cy.xpath('//*[@href="/manage/instructors/new"]').click()
-      cy.visit('https://lulu-s-school-031a.thinkific.com/manage/instructors/new')
+      cy.visit(Cypress.env('base_url') + '/manage/instructors/new')
       cy.url().should('include', '/manage/instructors/new')
-
+      //TITLE
       cy.get('.mobile-title-bar__title').should('have.text', 'Create a new instructor')
-
+      //INPUT FIELDS
       cy.get('#instructor_first_name')
-        .type('Matthew')
-        .should('have.value', 'Matthew')
+        .type(Cypress.env('test_instructor_first_name'))
+        .should('have.value', Cypress.env('test_instructor_first_name'))
       cy.get('#instructor_last_name')
-        .type('Richardson')
-        .should('have.value', 'Richardson')
+        .type(Cypress.env('test_instructor_last_name'))
+        .should('have.value', Cypress.env('test_instructor_last_name'))
       cy.get('#instructor_email')
-        .type('mattrichardson+qatest@thinkific.com')
-        .should('have.value', 'mattrichardson+qatest@thinkific.com')
+        .type(Cypress.env('test_instructor_email'))
+        .should('have.value', Cypress.env('test_instructor_email'))
       cy.get('#instructor_title')
-        .type('Senior Instructor')
-        .should('have.value', 'Senior Instructor')
-
+        .type(Cypress.env('test_instructor_title'))
+        .should('have.value', Cypress.env('test_instructor_title'))
+      //SAVE BUTTON
       cy.xpath('//*[@class="button-submit button button--primary save-button"]').click()
       cy.get('.mobile-title-bar__title').should('have.text', 'Edit instructor')
     
@@ -60,7 +52,7 @@
   })
   describe('Update Instructor Page', () => {
     it('Updated instructor', () => {
-      cy.visit("https://lulu-s-school-031a.thinkific.com/manage/instructors")
+      cy.visit(Cypress.env('base_url') + '/manage/instructors')
       cy.url().should('include', '/manage/instructors')
       cy.contains('Matthew')
       cy.contains('Richardson')
@@ -71,12 +63,13 @@
 
   describe('Search for Instructor', () => {
     it('Results include added instructor', () => {
-      cy.visit("https://lulu-s-school-031a.thinkific.com/manage/instructors")
+      cy.visit(Cypress.env('base_url') + '/manage/instructors')
       cy.url().should('include', '/manage/instructors')
+      //SEARCH FIELD
       cy.get('#q')
         .type('Matthew')
         .should('have.value', 'Matthew')
-
+      //SEARCH BUTTON
       cy.get('#btn-search').click() 
       cy.contains('Matthew')
     })
